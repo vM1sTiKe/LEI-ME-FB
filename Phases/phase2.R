91ea81e3c060dff62c21678d6edfd40acddb8881
Bruno Daniel <48827010+vM1sTiKe@users.noreply.github.com>
2025-04-14 22:55:46 +0100

    .

--- Phases/phase2.R
+++ Phases/phase2.R
@@ -12,7 +12,7 @@ source(here("scripts", "FBMetrics_Functions.r"))
 palette(c("#b30000", "#7c1158", "#4421af", "#1a53ff", "#0d88e6", "#00b7c7", "#5ad45a", "#8be04e", "#ebdc78", "lightgray"))
 
 
-# ESTUDO -----------------------------------------------------------------------
+# ESTUDO das variaveis estatisticas --------------------------------------------
 
 # Type
 calc_freq_df(Posts$Type, cumulative = FALSE)
